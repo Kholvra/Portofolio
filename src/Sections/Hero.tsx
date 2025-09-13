74349d4303b3b3c8719abf96fc536437edea645d
kolvra <sabrisirajkholbiabby@gmail.com>
2025-09-13 09:43:05 +0700

    feat(hero): build initial hero section component

--- src/Sections/Hero.tsx
+++ src/Sections/Hero.tsx
@@ -1,5 +1,18 @@
-function Hero (){
-    return
+function Hero() {
+  return (
+    <section className="flex flex-col gap-5 justify-center items-center h-screen m-auto">
+      <h1 className="text-8xl font-bold text-center">SABRI SIRAJ KHOLBI</h1>
+      <span className="text-3xl text-center font-medium">
+        Built with Code & Clarity.
+      </span>
+      <p className=" max-w-2xl mx-auto leading-relaxed text-center text-base text-gray-500">
+        I'm a software engineer who transforms complex problems into elegant
+        digital solutions. Specializing in modern web technologies, I craft
+        applications that balance technical excellence with exceptional user
+        experiences.
+      </p>
+    </section>
+  );
 }
 
 export default Hero;
