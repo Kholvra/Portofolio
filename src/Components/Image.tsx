ef92dd87ac4f37c334706df7288929fca9543677
kolvra <sabrisirajkholbiabby@gmail.com>
2025-09-14 15:37:24 +0700

    chore: update assets and minor component tweaks

--- src/Components/Image.tsx
+++ src/Components/Image.tsx
@@ -1,12 +1,15 @@
 type Props={
     source?: string
     grayscale?: boolean
+    brightnessFull?:boolean
+    isCrop?:boolean
 }
 
 
-function Image({source,grayscale}:Props){
-    const grayscaleOption = grayscale? "lg:grayscale lg:hover:grayscale-0" : "brightness-70 hover:brightness-100"
-    return <img src={source} alt="" className={`size-full rounded-lg ${grayscaleOption} transition duration-300`}/>
+function Image({source,grayscale,brightnessFull,isCrop}:Props){
+    const size = isCrop? "h-54 w-full object-cover" : "size-full"
+    const variant = grayscale? "lg:grayscale lg:hover:grayscale-0" : brightnessFull? "brightness-100" :"brightness-70 hover:brightness-80"
+    return <img src={source} alt="" className={`${size} rounded-lg ${variant} transition duration-300`}/>
 }
 
 export default Image;
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,6 @@ import myFace from "./myFace.jpeg";
 import pomodoroTimer from "./pomodorotimer.png";
 import inventoryManagement from "./inventoryManagement.png"
 import aquaticToDoList from "./aquaticToDoList.png"
+import close from "./close.png"
 
-export { myFace, pomodoroTimer,inventoryManagement, aquaticToDoList };
+export { myFace, pomodoroTimer,inventoryManagement, aquaticToDoList,close };
